204017718c37310f7031e0eba26722202af17ced
German Escobar <german.e.escobar@gmail.com>
2022-04-14 23:36:15 -0300

    integration iframe api jitsi

--- resources/views/room.blade.php
+++ resources/views/room.blade.php
@@ -1,7 +1,9 @@
+<script src='https://meet.jit.si/external_api.js'></script>
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Room') }}
+            {{ __('Sala') }}
         </h2>
     </x-slot>
 
@@ -9,9 +11,28 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    sala
+                    
+                    <div id="meet" class="w-full h-full"></div>
+                    
                 </div>
             </div>
         </div>
     </div>
 </x-app-layout>
+
+<script>
+    const domain = 'meet.jit.si';
+    const options = {
+        roomName: 'LaravelJitsiExample',
+        // width: 800,
+        // height: 700,
+        parentNode: document.querySelector('#meet'),
+        lang: 'es',
+        userInfo: {
+            email: @json(auth()->user()->email),
+            displayName: @json(auth()->user()->name),
+        }
+    };
+    const api = new JitsiMeetExternalAPI(domain, options);
+    // api.dispose();
+</script>
